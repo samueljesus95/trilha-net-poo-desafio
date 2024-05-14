d03c1d1a8f9110fe40355b9273e5157b446d4185
Samuel Jesus <samuejesus@live.com>
2024-05-14 09:58:02 -0300

    feat: implementando os TODOs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("123456789", "Nokia V2", "IMEI1234", 128);
+Iphone iphone = new Iphone("987654321", "iPhone 13", "IMEI4321", 128);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Nokia App");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("iPhone App");
